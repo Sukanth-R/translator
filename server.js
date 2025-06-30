e6f2b188a352a3afd2fba14cea447844c39f1aab
Sukanth-R <124667330+Sukanth-R@users.noreply.github.com>
2025-06-30 11:34:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -238,7 +238,12 @@ app.delete("/api/products/:id", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.get("/", (req, res) => {
+  res.send("Automax server is live 🚀");
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
